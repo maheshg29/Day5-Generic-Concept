923b8a904d4ecd70776fdc553bdbbb6ea110b841
mahesh <gangurdemahesh9@gmail.com>
2023-03-23 15:24:35 +0530

    [mahesh]add program file for delete Int Element

---
+++ Day5-GenericConcept/DeleteElement.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_GenericConcept
+{
+    public class DeleteElement
+    {
+        public static int size;
+        public static void DeleteIntElement(int[] arry, int delete)
+        {
+            int k = 0;
+            int[] result = new int[arry.Length - 1];
+
+            for (int i = 0; i < arry.Length; i++)
+            {
+                if (delete != arry[i])
+                {
+                    result[k] = arry[i];
+                    k++;
+                }
+            }
+            Console.WriteLine("\nResult After Delete Elemnet ");
+            foreach (int j in result)
+            {
+                Console.Write(j + " ");
+            }
+        }
+
+        public static int[] GetIntElement()
+        {
+            Console.WriteLine("Enter How many elemet u want to add");
+            DeleteElement.size = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[DeleteElement.size];
+            Console.WriteLine("Please enter element one by one");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            return arr;
+        }
+    }
+}
+
+
--- Day5-GenericConcept/Program.cs
+++ Day5-GenericConcept/Program.cs
@@ -11,6 +11,18 @@ namespace Day5_GenericConcept
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Generics Concepts Problems");
+            Console.WriteLine("Select any one option \n 1. Delete Int Element ");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    int[] intArry = DeleteElement.GetIntElement();
+                    Console.WriteLine("Plese pick up any one elemnt");
+                    int delete = Convert.ToInt32(Console.ReadLine());
+                    DeleteElement.DeleteIntElement(intArry, delete);
+                    break;
+            }
         }
     }
 }
